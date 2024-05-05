b05542f5b61046f5510b7c61c514a2446c9fabbd
Tahir Umar <tahirumardec@gmail.com>
2024-05-05 07:42:28 +0100

    initial commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name=input("enter your name: ")
+age=input("enter age: ")
+age=int(age)
+location=input("enter location: ")
+message=f"Hello {name.title()}, your are {age} years old, and your are living in {location.title()} "
+print(message)
